60e8d9ab7b200c8b12d8f21f7ee3140c589f1204
reginals674 <lopregi08@gmail.com>
2025-11-20 08:02:24 -0600

    prototipo inicial del proyecto

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 // Registrar Service Worker
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('/matrixland/sw.js')
       .then(registration => {
         console.log('SW registrado:', registration);
       })
